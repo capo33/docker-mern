afec0e3c0f3e6f1c3c6f7d6186bf141e3380c1f0
Mohamed <s2000588@edu.bc.fi>
2023-09-09 14:01:20 +0300

    install rimraf package

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,11 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
-app.listen(5000, () => console.log("Server running on port " + 5000));
+app.get("", (req: Request, res: Response) => {
+  res.json({
+    status: "success!!!",
+  });
+});
+
+app.listen(4000, () => console.log("Server running on " + 4000));
